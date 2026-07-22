4aa9848837ccb62ab25912cc3b250029e9536b39
marlozing <marlozing1234@gmail.com>
2026-07-22 15:05:26 +0900

    fix: Places 테이블 이름 변경

--- src/main/java/gdg/hongik/loca/entity/Place.java
+++ src/main/java/gdg/hongik/loca/entity/Place.java
@@ -8,7 +8,7 @@ import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 
 @Entity
-@Table(name = "places")
+@Table(name = "public_places")
 @Getter
 @Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
